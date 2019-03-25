1be22ff5d801c935b1a4706e5180160ff1bb15ea
user <user@users-MacBook-Pro.local>
2019-03-25 23:58:05 +0100

    Initial Commit

---
+++ Tutorial Alamofire And SwiftyJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tutorial Alamofire And SwiftyJSON
+//
+//  Created by user on 3/25/19.
+//  Copyright © 2019 Pekshn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
